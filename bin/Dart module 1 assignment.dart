860ac0b926334ce9248afd1d0932270485f531c6
Md Ebrahim Ahmed <ibrahimahmed52529@gmail.com>
2023-08-03 00:12:53 +0600

    Dart Module 1 Assignment

---
+++ bin/Dart module 1 assignment.dart
@@ -0,0 +1,17 @@
+void main(){
+  const int a= 70;
+  const int b= 4;
+  int u= a+b;
+  int v= a-b;
+  int w= a*b;
+  double x= a/b;
+  int y= a%b;
+
+  print(a);
+  print(b);
+  print(u); //Addition
+  print(v); //Subtraction
+  print(w); //Multiplication
+  print(x); //Division
+  print(y); //Modulus
+}
