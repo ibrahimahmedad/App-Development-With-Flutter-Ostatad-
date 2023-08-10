de2642febe33b071ae42ccfca6aadd0864d1ea4f
Md Ebrahim Ahmed <ibrahimahmed52529@gmail.com>
2023-08-11 00:56:23 +0600

    dart module 2 assignment

---
+++ bin/oatad_dart_module_2_assignment.dart
@@ -0,0 +1,28 @@
+void main(){
+
+  List gradesList =[90,85,80,95,64,70];
+
+  int totalGrades=
+      gradesList.elementAt(0)+
+          gradesList.elementAt(1)+
+          gradesList.elementAt(2)+
+          gradesList.elementAt(3)+
+          gradesList.elementAt(4)+
+          gradesList.elementAt(5);
+
+  double totalGradesAverage= totalGrades*100/600;
+
+  if (totalGradesAverage>=70){
+    print('Student average grade : ${totalGradesAverage.toStringAsFixed(2)}');
+
+    print('Round average: ${totalGradesAverage.toInt()}');
+
+    print('Passad');
+
+  }
+
+  else{
+    print('Failed');
+  }
+
+}
